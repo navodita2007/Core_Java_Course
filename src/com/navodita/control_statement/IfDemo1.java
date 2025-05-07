fb0dbe578b4a2fac9e87ba4a2557a865f7b5c4b9
Navodita <navoditaranchi@gmail.com>
2025-05-07 11:14:37 +0300

    If statement programs

---
+++ src/com/navodita/control_statement/IfDemo1.java
@@ -0,0 +1,17 @@
+package com.navodita.control_statement;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class IfDemo1 {
+    public static void main(String[] args) {
+        int no1 = 10;
+        int no2 ;
+
+        if(true) {
+            no2 = 20;
+            System.out.println("hello");
+        }
+
+        System.out.println(no2);
+    }
+}
