4c6dc15fcbdd25a7ef3d6fc7a0b628df691d7e3e
Navodita <navoditaranchi@gmail.com>
2025-07-14 15:45:49 +0300

    Program regarding single inheritance

--- src/com/navodita/oops/SingleInheritance.java
+++ src/com/navodita/oops/SingleInheritance.java
@@ -1,12 +1,12 @@
 package com.navodita.oops;
 
-class A {                // Parent class
+class A {                // class A is a Parent class. By default, Parent class A directly inherits object class.
     void show1() {
         System.out.println("I am in class A");
     }
 }
 
-class B extends A {          // class B is a child class
+class B extends A {          // class B is a child class.
     void show2() {
         System.out.println("I am in class B");
     }
@@ -24,3 +24,6 @@ public class SingleInheritance {
     }
 }
 /* In single inheritance child class will get/inherit all the properties of parent class. */
+/* By default, any class in java directly or indirectly inherits the object class. Any Parent class directly inherits
+the object class. Any child class indirectly inherits the object class.
+ */
