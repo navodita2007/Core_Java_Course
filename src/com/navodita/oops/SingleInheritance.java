87c8d389e32c730cb9f689ebddd92ac0f0e17ef7
Navodita <navoditaranchi@gmail.com>
2025-07-14 12:41:45 +0300

    Program regarding single inheritance

---
+++ src/com/navodita/oops/SingleInheritance.java
@@ -0,0 +1,26 @@
+package com.navodita.oops;
+
+class A {                // Parent class
+    void show1() {
+        System.out.println("I am in class A");
+    }
+}
+
+class B extends A {          // class B is a child class
+    void show2() {
+        System.out.println("I am in class B");
+    }
+}
+
+public class SingleInheritance {
+    public static void main(String[] args) {
+        A ob1 = new A();
+        ob1.show1();
+        // ob1.show2();    -> Can not write like this because parent class can't inherit the properties of child class.
+
+        B ob2 = new B();
+        ob2.show2();
+        ob2.show1();
+    }
+}
+/* In single inheritance child class will get/inherit all the properties of parent class. */
